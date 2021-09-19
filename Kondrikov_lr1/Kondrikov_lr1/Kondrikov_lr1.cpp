e80f4acb77d85ec9cdb63da85b642e48877a9511
DeLeshka <gladx01@mail.ru>
2021-09-20 01:58:44 +0300

    final

--- Kondrikov_lr1/Kondrikov_lr1/Kondrikov_lr1.cpp
+++ Kondrikov_lr1/Kondrikov_lr1/Kondrikov_lr1.cpp
@@ -3,11 +3,15 @@
 #include <string>
 #include <cassert>
 
+unsigned char (*codePtr)(int letter, int codeLetter);
+
 inline std::string getString(std::string message)
 {
     std::cout << "Please, enter " << message << ": ";
     std::string key;
-    std::getline(std::cin, key);
+    //std::cin.clear();
+    //std::cin.ignore(32767, '\n');
+    std::cin >> key;
     if (key == "") 
         key = getString(message);
     return key;
@@ -20,48 +24,102 @@ unsigned char encode(int letter, int codeLetter)
     letter += codeLetter - 1;
 
     //if letter's code will be > 254 we'll reduce it by 223 to insert it in [32, 254] interval
-    if (letter > 254) letter -= 223;
+    if (letter > 255) letter -= 224;
     return letter;
+}
+
+unsigned char decode(int letter, int codeLetter)
+{
+    if (codeLetter < 0) codeLetter += 256;
+    //chars are stored in memory as integers, so we can calculate a new one by ascii codes of original and current key letter
+    letter -= codeLetter - 1;
+
+    //if letter's code will be > 254 we'll reduce it by 223 to insert it in [32, 254] interval
+    if (letter < 32) letter += 224;
+    return letter;
+}
+
+int getAnswer()
+{
+    int answer;
+    std::cout << "Select an operation: ";
+    while ((std::cin >> answer).fail() || !(answer >= 0 && answer < 3))
+    {
+        std::cin.clear();
+        std::cin.ignore(32767, '\n');
+        std::cout << "Please, input a valid answer: ";
+    }
+    return answer;
 }
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    std::string key{ getString("key") };
+    while (true)
+    {
+        std::cout << "1. Encrypt a file.\n" << "2. Decrypt a file\n" << "0. Exit\n";
 
-    /*for (int i = 32; i < 255; i++)
-        std::cout << i << ' ' << static_cast<char>(i) << std::endl;*/
-
-    std::string inFName{getString("an input file name (without \".txt\")") + ".txt"};
-    std::ifstream inFile(inFName);
-    assert(inFile && "Output file couldn't be opened");
-
-    std::string outFName{ getString("an output file name (without \".txt\")") + ".txt" };
-    std::ofstream outFile(outFName);
-    assert(outFile && "Output file couldn't be opened");
-
-    int letter;
-    int i = 0;
-    int key_length = key.length();
-
-    while ((letter = inFile.get()) != EOF) {
-        if (letter != '\n')
+        switch (getAnswer())
         {
-
-            //for checking in console
-            //std::cout << letter << ' ' << (char)letter << ' ' << (int)encode(letter, static_cast<int>(key[i])) << ' ' << encode(letter, static_cast<int>(key[i])) << std::endl; 
-
-            outFile << encode(letter, static_cast<int>(key[i]));
-            i++;
-            if (i == key_length) i = 0;
+        case 1:
+        {
+            codePtr = encode;
+            break;
         }
-        else
-            outFile << std::endl;
+        case 2:
+        {
+            codePtr = decode;
+            break;
+        }
+        case 0:
+            return 0;
+        }
+
+        std::string key{ getString("key") };
+
+        std::string inFName{ getString("an input file name (without \".txt\")") + ".txt" };
+        std::ifstream inFile(inFName);
+        assert(inFile && "Output file couldn't be opened");
+
+        std::string outFName{ getString("an output file name (without \".txt\")") + ".txt" };
+        std::ofstream outFile(outFName);
+        assert(outFile && "Output file couldn't be opened");
+
+        int letter;
+        int i = 0;
+        int key_length = key.length();
+
+        while ((letter = inFile.get()) != EOF) {
+            if (letter != '\n')
+            {
+
+                //for checking in console
+                //std::cout << letter << ' ' << (char)letter << ' ' << (int)encode(letter, static_cast<int>(key[i])) << ' ' << encode(letter, static_cast<int>(key[i])) << std::endl; 
+
+                outFile << (*codePtr)(letter, static_cast<int>(key[i]));
+                i++;
+                if (i == key_length) i = 0;
+            }
+            else
+                outFile << std::endl;
+        }
+
+        std::cout << "\nEncryption was succesfull.";
+        inFile.close();
+        outFile.close();
     }
 
-    std::cout << "\nEncryption was succesfull.";
-    inFile.close();
-    outFile.close();
+
+
+
+
+
+
+
+
+
+
+
 
 
     //checking prowed that symbol codes in output file are the same, so decrypting is real
